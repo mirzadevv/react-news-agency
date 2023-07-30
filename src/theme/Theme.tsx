ed34a6eb9be347c3c7002899d312e7f482a572c9
Eqbal Mirzaei <mirzaei.devv@gmail.com>
2023-07-31 01:00:35 +0430

    feat: Create a theme file to centralize the necessary themes.
    The colors and fonts have been added to the theme object

---
+++ src/theme/Theme.tsx
@@ -0,0 +1,14 @@
+const theme = {
+  colors: {
+    pageBackground: "#eeebf5",
+    lightBlack: "#4c455f",
+    darkBlack: "#26222f",
+    purple: "#805aed",
+  },
+  fonts: {
+    inter: "Inter, sans-serif",
+    sora: "Sora, sans-serif",
+  },
+};
+
+export default theme;
