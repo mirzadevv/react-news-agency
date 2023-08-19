e1c0cb0baa0e1b066b9a5b8b5c3c55bca75a3778
Eqbal Mirzaei <mirzaei.devv@gmail.com>
2023-08-19 22:02:44 +0430

    feat/implement navbarAuthentication component
    This componet is used for showing the authentication part of the navbar

---
+++ src/components/common/navbar/navbarAuthentication/index.tsx
@@ -0,0 +1,14 @@
+import * as Styled from "./index.styled";
+
+export default function NavbarAuthentication() {
+  return (
+    <Styled.Container>
+      <li>
+        <a>Login</a>
+      </li>
+      <li>
+        <a>Subscribe</a>
+      </li>
+    </Styled.Container>
+  );
+}
