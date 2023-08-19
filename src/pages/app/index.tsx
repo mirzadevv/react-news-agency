5a9d4a0cc07263ea57e8d6b574ec98861158e415
Eqbal Mirzaei <mirzaei.devv@gmail.com>
2023-08-19 22:18:05 +0430

    feat: add the queryClinetProvider of react query to the project
    The header component has been added as well

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -1,17 +1,23 @@
 import React from "react";
 import * as Styled from "./index.styled";
+import { QueryClient, QueryClientProvider } from "react-query"; // Import React Query modules
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "../../theme/GlobalStyles";
 import Theme from "../../theme/Theme";
+import Header from "../../components/header";
+
+const queryClient = new QueryClient();
 
 const App: React.FC = () => {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={Theme}>
         <GlobalStyles />
-        <Styled.Container></Styled.Container>
+        <Styled.Container>
+          <Header />
+        </Styled.Container>
       </ThemeProvider>
-    </>
+    </QueryClientProvider>
   );
 };
 
