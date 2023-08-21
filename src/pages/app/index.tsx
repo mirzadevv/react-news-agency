ee4afc4b8f57035df320bf2e8ac0492faab69857
Eqbal Mirzaei <mirzaei.devv@gmail.com>
2023-08-21 17:08:01 +0430

    feat: initialize the routes

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -1,10 +1,24 @@
+// import react-query related modules
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+// import theme related modules
 import GlobalStyles from "../../theme/GlobalStyles";
-import { ThemeProvider } from "styled-components";
-import Header from "../../components/header";
-import * as Styled from "./index.styled";
 import Theme from "../../theme/Theme";
+
+// import styled-component related modules
+import { ThemeProvider } from "styled-components";
+import * as Styled from "./index.styled";
+
+// import react-router-dom related modules
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+
+// import components
+import Navbar from "../../components/common/navbar";
+
+// import pages
+import CategoryPage from "../categoryPage";
+import HomePage from "../homePage";
 
 export default function App() {
   const queryClient = new QueryClient();
@@ -13,7 +27,36 @@ export default function App() {
       <ThemeProvider theme={Theme}>
         <GlobalStyles />
         <Styled.Container>
-          <Header />
+          <Router>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route
+                path="/category/world"
+                element={<CategoryPage columns={3} categoryTitle="world" />}
+              />
+              <Route
+                path="/category/business"
+                element={<CategoryPage columns={3} categoryTitle="business" />}
+              />
+              <Route
+                path="/category/tech"
+                element={<CategoryPage columns={3} categoryTitle="tech" />}
+              />
+              <Route
+                path="/category/science"
+                element={<CategoryPage columns={3} categoryTitle="science" />}
+              />
+              <Route
+                path="/category/lifestyle"
+                element={<CategoryPage columns={3} categoryTitle="lifestyle" />}
+              />
+              <Route
+                path="/category/politics"
+                element={<CategoryPage columns={3} categoryTitle="politics" />}
+              />
+            </Routes>
+          </Router>
         </Styled.Container>
       </ThemeProvider>
       <ReactQueryDevtools initialIsOpen={false} />
