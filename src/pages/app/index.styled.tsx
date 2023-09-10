f61f78ff6565f4fe4dc3f9a17327ccbe56be64ea
Eqbal Mirzaei <mirzaei.devv@gmail.com>
2023-09-10 17:56:19 +0430

    style: edit padding and margin of the app and categoryPage
    It's been edited in different ways of media query

--- src/pages/app/index.styled.tsx
+++ src/pages/app/index.styled.tsx
@@ -1,9 +1,13 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  margin: 50px 150px;
+  padding: 50px 150px;
 
-  @media (max-width: 700px) {
-    margin: 50px;
+  @media (max-width: 900px) {
+    padding: 50px 100px;
+  }
+
+  @media (max-width: 600px) {
+    padding: 50px 50px;
   }
 `;
--- src/pages/categoryPage/index.styled.tsx
+++ src/pages/categoryPage/index.styled.tsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 export const Container = styled.div<{ columns: number }>`
   display: grid;
   grid-template-columns: repeat(${(props) => props.columns}, 1fr);
-  margin-top: 100px;
 
   .category-title {
     text-transform: uppercase;
