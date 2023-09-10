893727d38054af983d604467a0e772b224f4dc46
Eqbal Mirzaei <mirzaei.devv@gmail.com>
2023-09-10 17:53:17 +0430

    style: add a color to the theme object

--- src/theme/Theme.tsx
+++ src/theme/Theme.tsx
@@ -6,6 +6,7 @@ const theme: Types.Theme = {
     lightBlack: "#4c455f",
     darkBlack: "#26222f",
     purple: "#8100ea",
+    darkWhite: "#e1d9d1",
   },
   fonts: {
     inter: "Inter, sans-serif",
